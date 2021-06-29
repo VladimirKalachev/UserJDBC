2888322746d4ca6116e8527aa9712d0a1f584168
VladimirKalachev <arrrrgh@inbox.ru>
2021-06-29 12:59:39 +0300

    first version create table insert data into db get data from db to console

--- Main6.java
+++ Main6.java
@@ -13,71 +13,52 @@ public class Main6 {
 
     public static void main(String[] args) throws SQLException {
 
-        //registration jdbc
 
-        System.out.println("Testing connection to PostgreSQL");
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-            e.printStackTrace();
-            return;
-        }
-
-        System.out.println("PostgreSQL JDBC Driver successfully connected");
-
-        Connection connection = null;
-
-        //create connection to database
-
-        try {
-            connection = DriverManager
-                    .getConnection(DB_URL, USER, PASS);
-        } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            e.printStackTrace();
-            return;
-        }
-
-        if (connection != null) {
-            System.out.println("You successfully connected to database now");
-        } else {
-            System.out.println("Failed to make connection to database");
-        }
-
-        Statement statement = connection.createStatement();
-
-
-        String createSQLTable = "CREATE TABLE user" +
-                "(id INTEGER not NULL" +
-                "firstName VARCHAR(30)" +
-                "lastName VARCHAR(30)" +
-                "companyID INTEGER not NULL" +
+        String createSQLTable = "CREATE TABLE users" +
+                "(id INTEGER not NULL, " +
+                "firstName VARCHAR(30), " +
+                "lastName VARCHAR(30), " +
+                "companyID INTEGER not NULL, " +
                 "role VARCHAR(30))";
 
-        String insertUser1 = "INSERT INTO user VALUES(1, 'firstName1', 'lastName1', 4321, 'junior')";
-        String insertUser2 = "INSERT INTO user VALUES(2, 'firstName2', 'lastName2', 4321, 'junior')";
-        String insertUser3 = "INSERT INTO user VALUES(3, 'firstName3', 'lastName3', 4321, 'middle')";
-        String insertUser4 = "INSERT INTO user VALUES(4, 'firstName4', 'lastName4', 4321, 'lead')";
-        String insertUser5 = "INSERT INTO user VALUES(5, 'firstName5', 'lastName5', 4321, 'QA')";
+        String insertUser1 = "INSERT INTO users VALUES(1, 'firstName1', 'lastName1', 4321, 'junior')";
+        String insertUser2 = "INSERT INTO users VALUES(2, 'firstName2', 'lastName2', 4321, 'junior')";
+        String insertUser3 = "INSERT INTO users VALUES(3, 'firstName3', 'lastName3', 4321, 'middle')";
+        String insertUser4 = "INSERT INTO users VALUES(4, 'firstName4', 'lastName4', 4321, 'lead')";
+        String insertUser5 = "INSERT INTO users VALUES(5, 'firstName5', 'lastName5', 4321, 'QA')";
 
-        String getUser = "SELECT * FROM user";
+        String getUser = "SELECT id, firstName, lastName, companyID, role FROM users";
 
+    //create table users in training db
+    try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();){
+        statement.executeUpdate(createSQLTable);
+        System.out.println("Table create successfully");
+    }
 
-        //
-        try (Connection connectionCreateDB = DriverManager.getConnection(DB_URL, USER, PASS))
-            {
-                statement.executeUpdate(createSQLTable);
-            }catch (SQLException e){
-                e.printStackTrace();
+    //add entries into users table
+    try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();){
+        statement.executeUpdate(insertUser1);
+        System.out.println("Update table successfully");
+    }
+
+    //get entries from db to console
+    try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(getUser);){
+
+        while (resultSet.next()) {
+            System.out.println("ID: " + resultSet.getInt("id") +
+                    " First name: " + resultSet.getString("firstName") +
+                    " Last name: " + resultSet.getString("lastName") +
+                    " Company ID: " + resultSet.getString("companyID") +
+                    " Role: " + resultSet.getString("role"));
         }
-
-
-
-
+    }
     }
 }
+
 
 
 
